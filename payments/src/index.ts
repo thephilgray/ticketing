b9fa7df930685a440b3206bc0d636f2c23796dd3
thephilgray <thephilgray@gmail.com>
2020-08-24 23:18:07 -0400

    add pointless console.log in payments

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -6,6 +6,7 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 
 
 const start = async () => {
+    console.log('starting payments...');
     if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined.');
     }
