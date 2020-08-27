64382c96829d657f5b952880bfb505970139ceb2
thephilgray <thephilgray@gmail.com>
2020-08-26 20:35:40 -0400

    add fix for bug with ingress-nginx on Digital Ocean

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 export default ({ req }) => {
     const serverSideRequest = typeof window === 'undefined';
+    const isDev = process?.env?.NODE_ENV === 'development';
+    const serverSideBaseUrl = isDev ? 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local' : 'http://fauxios.com/';
     return axios.create({
-        baseURL: serverSideRequest ? 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local' : '',
+        baseURL: serverSideRequest ? serverSideBaseUrl : '',
         ...serverSideRequest && {
             headers: req.headers
         }
