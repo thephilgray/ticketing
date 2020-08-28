5006da0bd8ce5353eeb310db12a5a7fb81c4be2d
thephilgray <thephilgray@gmail.com>
2020-08-27 20:39:45 -0400

    change serverside base url for prod

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 export default ({ req }) => {
     const serverSideRequest = typeof window === 'undefined';
-    const isDev = process?.env?.NODE_ENV === 'development';
-    const serverSideBaseUrl = isDev ? 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local' : 'http://fauxios.com/';
+    const serverSideBaseUrl = 'http://fauxios.com/';
     return axios.create({
         baseURL: serverSideRequest ? serverSideBaseUrl : '',
         ...serverSideRequest && {
